14704ec9cb150db1e6250e4288420dafcb147059
Lolillya <SomeonesUsernam@gmail.com>
2023-06-07 17:28:43 +0800

    added background container sc

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+import { GradientBackgroundCon } from './components/QuoteGenerator/QuoteGeneratorElements'
+
+export default function App() {
+  return (
+    <>
+      <GradientBackgroundCon></GradientBackgroundCon>
+    </>
+  )
+}
